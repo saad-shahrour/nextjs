728905876f82a05305db84cbe3d0e1fa8e3de17b
saad-shahrour <m.saad.shahrour@gmail.com>
2023-07-11 20:27:43 +0300

    ss

--- app/page.js
+++ app/page.js
@@ -1,95 +1,64 @@
+'use client'
+import { client } from "@gradio/client";
+import React from "react";
 import Image from 'next/image'
-import styles from './page.module.css'
+import { useState } from 'react';
+import styles from "./page.module.css"
 
-export default function Home() {
-  return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+
+// Changed next.config.js a lot for the client side and server side stuff
+// Used Gradio and hugging face to create the model then using it as a API
+// used Blob and FileReader in order to fetch an image and display it from user
+
+
+function Home() {
+  
+  const [data, setData] = useState()
+  const [file, setFile] = useState(null);
+  const [fileDataURL, setFileDataURL] = useState(null);
+  const [loading, setLoading] = useState(false)
+
+  async function run(e) {
+    e.preventDefault()
+
+    setLoading(true)
+    const file = e.target.files[0]
+    const blob = new Blob([file],  { type: file.type })
+    setFile(file);
+
+    const app = await client("https://saadshahrour-classify.hf.space/");
+    const result = await app.predict("/predict", [
+          blob, 	// blob in 'img' Image component
+    ]);
+
+    setData(result.data)
+    console.log(result.data);
+    
+    
+    let fileReader = new FileReader();
+    fileReader.onload = (e) => {
+      const { result } = e.target;
+      if (result) {
+        setFileDataURL(result)
+      }
+    }
+    fileReader.readAsDataURL(file);
+
+    setLoading(false)
+  }
+
+  return ( 
+    <div className={styles.container}>
+      <div>
+        <input type="file" onChange={async(e) => run(e)} accept='.png, .jpg, .jpeg' value=''/>
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div>
+        {loading && "loading..."}
+        {(data && !loading) && <img src={fileDataURL} width={150} height={150} />}
+        {(data && !loading) && <h3>The image you uploaded is for a {data[0].label} </h3>}
       </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
   )
 }
+
+export default Home;
